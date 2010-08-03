024c6c41d177187d18bfc554fb4772c105dc96e7
adamw <adam@warski.org>
2010-08-03 14:50:32 +0200

    Ability to set an entity manager factory for testing, statically.

--- cdi/src/main/java/pl/softwaremill/cdiext/persistence/EntityManagerFactoryProducer.java
+++ cdi/src/main/java/pl/softwaremill/cdiext/persistence/EntityManagerFactoryProducer.java
@@ -9,12 +9,26 @@ import javax.persistence.PersistenceUnit;
  * @author Adam Warski (adam at warski dot org)
  */
 public class EntityManagerFactoryProducer {
+    private static EntityManagerFactory staticEntityManagerFactory;
+
     @PersistenceUnit
     private EntityManagerFactory entityManagerFactory;
 
     @Produces
     @RequestScoped
     public EntityManagerFactory getEntityManagerFactory() {
+        if (staticEntityManagerFactory != null) {
+            return staticEntityManagerFactory;
+        }
+
         return entityManagerFactory;
+    }
+
+    /**
+     * Set an entity manager factory for testing. 
+     * @param staticEntityManagerFactory Entity manager factory that will be returned by this producer.
+     */
+    public static void setStaticEntityManagerFactory(EntityManagerFactory staticEntityManagerFactory) {
+        EntityManagerFactoryProducer.staticEntityManagerFactory = staticEntityManagerFactory;
     }
 }
